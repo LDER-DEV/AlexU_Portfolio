69c9647af6aa17cc92698fe0f8d92768a5e974ad
LDER-DEV <ludjy.dev@gmail.com>
2025-04-13 18:48:17 -0400

    updated short film , added contact page, currently working on poetry (bg css)

---
+++ src/assets/shortFilmData.jsx
@@ -0,0 +1,13 @@
+const videoData = [
+    {
+      id: 'teSuN1xhIUQ',
+      title: 'Paradiso'
+    },
+    {
+        id: 'IG76Rf28MQA',
+        title: 'Place holder'
+      },
+    
+  ]
+
+  export default videoData
